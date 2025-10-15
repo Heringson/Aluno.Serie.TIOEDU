bc56b0879cb7848c3bf4f578b25018cc82e05ff6
Heringson <majorlazerhs@gmail.com>
2025-10-15 20:29:21 -0300

    Remove comments and streamline notas.ts

--- src/notas.ts
+++ src/notas.ts
@@ -1,7 +1,4 @@
-//Gabriel Caire Nomura 2501980
-//Heitor Matos da Silva 2520912
 //Heringson Lima 2404307
-//Rafael Ienne Manoel 2519853
 //Wesley da Silva Santos 2522594
 
 import * as fs from 'fs';
@@ -162,4 +159,5 @@ async function main() {
 }
 
 main();
+
 
